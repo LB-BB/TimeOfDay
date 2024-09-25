9e276108af122e98e55a6049afbda9c1ac417726
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-09-25 15:26:13 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ components/TimeOfDay/TimeOfDay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const TimeOfDay = () => {
+  return(
+    <Text>Hello Text!</Text>
+  );
+};
+
+export default TimeOfDay;
